95ef16b48c14ce36f446afc8f40c9f0087bc905c
M. Evan Anindya W <m.evan.aw@gmail.com>
2026-02-02 11:02:24 +0700

    Update wwwroot and controller.

---
+++ AodTrainingInProgress/Controllers/MonitorWebController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AodTrainingInProgress.Controllers
+{
+    public class MonitorWebController : Controller
+    {
+        [Route("monitor_web/collect")]
+        public IActionResult Info()
+        {
+            return NoContent();
+        }
+    }
+}
