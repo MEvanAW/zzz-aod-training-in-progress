1c4e92a2d339fd42977a1555b6933c5df51f8f63
M. Evan Anindya W <m.evan.aw@gmail.com>
2026-02-02 17:19:06 +0700

    Add verifyCookieToken.

---
+++ AodTrainingInProgress/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AodTrainingInProgress.Controllers
+{
+    [Route("account")]
+    public class AccountController : Controller
+    {
+        [Route("ma-passport/token/verifyCookieToken")]
+        public IActionResult VerifyCookieToken()
+        {
+            return Ok(new Dictionary<string, object?>{
+                { "data", null},
+                { "message", "Login status is invalid. Please log in again." },
+                { "retcode", -100 }
+            });
+        }
+    }
+}
